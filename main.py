fef6a6b7d661a5a4139fcea8c2d22118cfea7e5e
c0rals <martinezj.crls@gmail.com>
2022-06-06 00:03:59 -0400

    Arreglo de funcion para clickear en calendario agregada funcion para clickear el elemento de la lista por nombre

--- main.py
+++ main.py
@@ -11,15 +11,20 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
 driver.maximize_window()
-wait_short = WebDriverWait(driver, 20)
+wait_short = WebDriverWait(driver,20)
 wait_long = WebDriverWait(driver, 200)
 
 # Login
+
+
 def login(driver):
     driver.get("https://clinico.rayenaps.cl/")
-    Ubicacion_input = driver.find_element(By.ID, "location")
-    Usuario_input = driver.find_element(By.ID, "username")
-    Clave_input = driver.find_element(By.ID, "password")
+    Ubicacion_input = driver.find_element(
+        By.ID, "location")
+    Usuario_input = driver.find_element(
+        By.ID, "username")
+    Clave_input = driver.find_element(
+        By.ID, "password")
 
     Ubicacion_input.clear()
     Usuario_input.clear()
@@ -33,10 +38,8 @@ def login(driver):
     wait_short.until(EC.url_contains("main"))
 
     try:
-        wait_short.until(EC.presence_of_element_located(
-            (By.CLASS_NAME, "cache-loading")))
-        wait_long.until_not(EC.presence_of_element_located(
-            (By.CLASS_NAME, "cache-loading")))
+        wait_short.until(EC.presence_of_element_located((By.CLASS_NAME, "cache-loading")))
+        wait_long.until_not(EC.presence_of_element_located((By.CLASS_NAME, "cache-loading")))
 
         print("[+] Login success")
     except:
@@ -45,16 +48,18 @@ def login(driver):
     return driver
 
 # Navegate to list
+
+
 def goToTable(driver):
-    driver.find_element(By.ID, "navbar-main-menu").click()
-    driver.find_element(By.XPATH, "//*[contains(text(), 'Box')]").click()
+    driver.find_element(
+        By.ID, "navbar-main-menu").click()
+    driver.find_element(
+        By.XPATH, "//*[contains(text(), 'Box')]").click()
     driver.find_element(
         By.XPATH, "//*[contains(text(), 'Pacientes citados')]").click()
     try:
-        wait_short.until(EC.visibility_of_all_elements_located(
-            (By.CSS_SELECTOR, "div.ReactTable.table-height")))
-        wait_short.until(EC.visibility_of_all_elements_located(
-            (By.CSS_SELECTOR, "div.rt-thead.-header")))
+        wait_short.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.ReactTable.table-height")))
+        wait_short.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.rt-thead.-header")))
         print("[+] Table load success")
     except:
         print("[!] Table load fail")
@@ -62,29 +67,81 @@ def goToTable(driver):
     return driver
 
 # Get list of names (need to be valid names, not all names)
+
+
 def getNamesFromTable(driver):
-    table = driver.find_element(By.CSS_SELECTOR, "div.ReactTable.table-height")
-    tableElements = table.find_elements(By.XPATH, "//div/div[2]/div/div/div[3]")
+    try:
+        wait_short.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR,"div.rt-tr-group")))
+    except:
+        print("[!] Names not found")
+        return
+
+    tableElements = driver.find_elements(By.XPATH, "//*[@class='rt-table']/div[2]/div/div/div[3]")
     tableNames = []
     for element in tableElements:
         if(element.text != " "):
             tableNames.append(element.text)
     if(len(tableNames) == 0):
         print("[!] Names not found")
+        return
     else:
         print("[+] Names found successful")
-    
+
     return tableNames
 
 # Search answers from excel by name
+
+
 def answersForName(nameList):
     for name in nameList:
         nameParsed = excel.parseData(name)
-        print("-" + nameParsed + "-")
+        print(nameParsed)
         print(excel.find_data(nameParsed))
+
+
+def click_calendar(driver,day):
+    try:
+        wait_short.until(EC.visibility_of_element_located((By.XPATH,"//*[@id='root']/div/div[1]/div[3]/div[1]/div/span[2]")))
+    except:
+        print("[!] Title bar not found")
+        return
+
+    try:
+        wait_short.until(EC.element_to_be_clickable((By.XPATH,"//*[@id='root']/div/div[1]/div[3]/div[1]/div/span[2]/div/div")))
+        driver.find_element(By.XPATH,"//*[@id='root']/div/div[1]/div[3]/div[1]/div/span[2]/div/div").click()
+        print("[+] Calendar clicked")
+        try:
+            wait_short.until(EC.element_to_be_clickable((By.CLASS_NAME,"react-datepicker__month")))
+            driver.find_element(By.XPATH,"//div[@class='react-datepicker__week']/div[contains(text(),'"+day+"')]").click()
+            print("[+] Day "+day+" clicked")
+        except:
+            print("[!] Day not clicked")
+    except:
+        print("[!] Calendar not found")
+        return
+
+
+def clickName(driver,name):
+    try:
+        wait_short.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR,"div.rt-tr-group")))
+    except:
+        print("[!] Names not found")
+        return
+
+    tableElements = driver.find_elements(By.XPATH, "//*[@class='rt-table']/div[2]/div/div/div[3]")
+
+    for element in tableElements:
+        if(name in element.text):
+            element.click()
+            print("[+] "+name+" clicked")
+            return
+    print("[!] "+name+" not found")
+    return
+
 
 if __name__ == '__main__':
     login(driver)
     goToTable(driver)
-    names = getNamesFromTable(driver)
-    answersForName(names)
+    click_calendar(driver,"8")
+    # names = getNamesFromTable(driver)
+    # answersForName(names)
